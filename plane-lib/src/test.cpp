d22441211f35c6de8a74157ac6df1739247b0492
evlc <dodgedodgedoggy@gmail.com>
2026-02-22 23:26:27 +0700

    plane-lib: update tests

--- plane-lib/src/test.cpp
+++ plane-lib/src/test.cpp
@@ -4,43 +4,45 @@
 #include <optional>
 
 TEST(PlaneLibTest, VPParallelAreParallel) {
-  Line alpha(Vec(1, 2), {1, 2});
-  Line beta(Vec(2, 4), {19, -28});
+  Line alpha = *Line::from_dir_and_point(Vec(1, 2), {1, 2});
+  Line beta = *Line::from_dir_and_point(Vec(2, 4), {19, -28});
 
   EXPECT_TRUE(alpha.parallel(beta));
 }
 
 TEST(PlaneLibTest, VPParallelDoesNotIntersect) {
-  Line alpha(Vec(1, 2), {1, 2});
-  Line beta(Vec(2, 4), {19, -28});
+  Line alpha = *Line::from_dir_and_point(Vec(1, 2), {1, 2});
+  Line beta = *Line::from_dir_and_point(Vec(2, 4), {19, -28});
   auto intersection = alpha.intersection(beta);
   EXPECT_FALSE(intersection.has_value());
 }
 
 TEST(PlaneLibTest, VPNonParallelIntersect) {
-  Line alpha(Vec(1, 1), {1, 2});
-  Line beta(Vec(2, 4), {19, -28});
+  Line alpha = *Line::from_dir_and_point(Vec(1, 1), {1, 2});
+  Line beta = *Line::from_dir_and_point(Vec(2, 4), {19, -28});
   auto intersection = alpha.intersection(beta);
   EXPECT_TRUE(intersection.has_value());
 }
 
 TEST(PlaneLibTest, CFParallelAreParallel) {
-  Line alpha(3, 19);
-  Line beta(3, 98);
+  Line alpha = *Line::from_constants(3, 19);
+  Line beta = *Line::from_constants(3, 98);
 
   EXPECT_TRUE(alpha.parallel(beta));
 }
 
 TEST(PlaneLibTest, CFParallelDoesNotIntersect) {
-  Line alpha(3, 19);
-  Line beta(3, 98);
+  Line alpha = *Line::from_constants(3, 19);
+  Line beta = *Line::from_constants(3, 98);
+
   auto intersection = alpha.intersection(beta);
   EXPECT_FALSE(intersection.has_value());
 }
 
 TEST(PlaneLibTest, CFNonParallelIntersect) {
-  Line alpha(1, 0);
-  Line beta(2, 0);
+  Line alpha = *Line::from_constants(1, 0);
+  Line beta = *Line::from_constants(2, 0);
+
   auto intersection = alpha.intersection(beta);
   EXPECT_TRUE(intersection.has_value());
 }
